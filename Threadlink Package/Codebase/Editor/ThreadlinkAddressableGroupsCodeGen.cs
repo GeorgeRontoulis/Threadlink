e0fbc9ca31933cfc046efb00db5e4baa16ff34a4
WhiteFawks <g.rontoulis@gmail.com>
2025-05-23 03:58:25 +0300

    Fixed wrong context menu

--- Threadlink Package/Codebase/Editor/ThreadlinkAddressableGroupsCodeGen.cs
+++ Threadlink Package/Codebase/Editor/ThreadlinkAddressableGroupsCodeGen.cs
@@ -1,11 +1,11 @@
 namespace Threadlink.Editor
 {
-	using System;
-	using System.Collections.Generic;
-	using System.IO;
-	using UnityEditor;
-	using UnityEditor.AddressableAssets;
-	using UnityEngine;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using UnityEditor;
+    using UnityEditor.AddressableAssets;
+    using UnityEngine;
 
 #if ODIN_INSPECTOR
 	using Sirenix.OdinInspector;
@@ -13,62 +13,62 @@ namespace Threadlink.Editor
 	using Threadlink.Editor.Attributes;
 #endif
 
-	[CreateAssetMenu(menuName = "Threadlink/Core/Addressable Groups Generator")]
-	internal sealed class ThreadlinkAddressableGroupsCodeGen : ScriptableObject
-	{
+    [CreateAssetMenu(menuName = "Threadlink/Core/Addressable Groups Generator")]
+    internal sealed class ThreadlinkAddressableGroupsCodeGen : ScriptableObject
+    {
 #if ODIN_INSPECTOR || THREADLINK_INSPECTOR
 		[ReadOnly]
 #endif
-		[SerializeField] private TextAsset template = null;
+        [SerializeField] private TextAsset template = null;
 
 #if ODIN_INSPECTOR || THREADLINK_INSPECTOR
 		[ReadOnly]
 #endif
-		[SerializeField] private MonoScript pointersScript = null;
+        [SerializeField] private MonoScript pointersScript = null;
 
-		[Space(10)]
+        [Space(10)]
 
 #if ODIN_INSPECTOR || THREADLINK_INSPECTOR
 		[ReadOnly]
 #endif
-		[SerializeField]
-		private List<string> discoveredAddressableGroups = new();
+        [SerializeField]
+        private List<string> discoveredAddressableGroups = new();
 
 #if ODIN_INSPECTOR
 		[Button]
 #else
-		[ContextMenu("Generate Custom Event Signatures")]
+        [ContextMenu("Generate Addressable Group Signatures")]
 #endif
 #pragma warning disable IDE0051
-		private void GenerateAddressableGroupSignatures()
-		{
-			string templateContent = template.text;
-			string separator = "," + Environment.NewLine;
+        private void GenerateAddressableGroupSignatures()
+        {
+            string templateContent = template.text;
+            string separator = "," + Environment.NewLine;
 
-			var settings = AddressableAssetSettingsDefaultObject.GetSettings(false);
+            var settings = AddressableAssetSettingsDefaultObject.GetSettings(false);
 
-			if (settings == null) throw new NullReferenceException("There is no Addressable Settings asset in the project!");
+            if (settings == null) throw new NullReferenceException("There is no Addressable Settings asset in the project!");
 
-			var groups = settings.groups;
+            var groups = settings.groups;
 
-			discoveredAddressableGroups.Clear();
+            discoveredAddressableGroups.Clear();
 
-			for (int i = 0; i < groups.Count; i++)
-			{
-				var groupName = groups[i].name;
+            for (int i = 0; i < groups.Count; i++)
+            {
+                var groupName = groups[i].name;
 
-				if (groupName.Equals("Built In Data") || groupName.Contains("Localization")) continue;
+                if (groupName.Equals("Built In Data") || groupName.Contains("Localization")) continue;
 
-				discoveredAddressableGroups.Add(groupName.Replace(" ", string.Empty).Replace("-", "_"));
-			}
+                discoveredAddressableGroups.Add(groupName.Replace(" ", string.Empty).Replace("-", "_"));
+            }
 
-			templateContent = templateContent.Replace("{CustomEntries}", string.Join(separator, discoveredAddressableGroups));
+            templateContent = templateContent.Replace("{CustomEntries}", string.Join(separator, discoveredAddressableGroups));
 
-			File.WriteAllText(string.Join("/", Path.GetDirectoryName(AssetDatabase.GetAssetPath(pointersScript)), "ThreadlinkAddressableGroups.cs"),
-			CSharpier.CodeFormatter.Format(templateContent).Code);
+            File.WriteAllText(string.Join("/", Path.GetDirectoryName(AssetDatabase.GetAssetPath(pointersScript)), "ThreadlinkAddressableGroups.cs"),
+            CSharpier.CodeFormatter.Format(templateContent).Code);
 
-			AssetDatabase.Refresh();
-			AssetDatabase.SaveAssets();
-		}
-	}
+            AssetDatabase.Refresh();
+            AssetDatabase.SaveAssets();
+        }
+    }
 }
