4bf165f916e7f1d2ab5c608c118e3e50d69e3455
WhiteFawks <g.rontoulis@gmail.com>
2026-01-02 06:34:51 +0200

    Removed Unused user constants

--- Threadforge/Threadlink User/Codebase/Constants.User.cs
+++ Threadforge/Threadlink User/Codebase/Constants.User.cs
@@ -1,12 +1,4 @@
 namespace Threadlink.User
 {
-    public static class UserConstants
-    {
-        public static class FilePaths
-        {
-#if UNITY_EDITOR
-            public const string IRIS_USER_EVENTS_PATH = "Assets/Threadforge/Threadlink User/Codebase/Iris.Events.User.txt";
-#endif
-        }
-    }
+    public static class UserConstants { }
 }
